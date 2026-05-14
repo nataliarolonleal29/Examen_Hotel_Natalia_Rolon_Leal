a08c55d73cb0e45cf1f0a038ea3356804061f8cf
none <none>
2026-05-14 12:52:20 -0500

    feat: termine las cards simples, sara

---
+++ components/room-card.js
@@ -0,0 +1,25 @@
+import { habitaciones } from "./habitaciones.js";
+console.log(habitaciones);
+const contenedor = document.getElementById("contenedor-habitaciones")
+habitaciones.forEach(habitacion => {
+
+    contenedor.innerHTML += `
+    
+      <div class="card-completa">
+  
+        <img src="${habitacion.imagen}">
+  
+        <h2>${habitacion.nombre}</h2>
+  
+        <p>${habitacion.camas} camas</p>
+  
+        <p>Capacidad: ${habitacion.personas}</p>
+  
+        <p class="precio">$${habitacion.precio}</p>
+  
+        <button>Reservar</button>
+  
+      </div>
+  
+    `
+  })
--- js/habitaciones.js
+++ js/habitaciones.js
@@ -1,5 +1,4 @@
-const contenedor = document.getElementById("contenedor-habitaciones")
-const habitaciones = [
+export const habitaciones = [
   {
     id: 1,
     nombre: "Suite Familiar",
@@ -7,8 +6,11 @@ const habitaciones = [
     personas: 4,
     baño: "Tina y regadera",
     precio: 320,
+    descripcion: "Habitación amplia y cómoda ideal para familias. Cuenta con espacios modernos y ambiente acogedor para una estadía relajante.",
     servicios: ["Wifi", "Jacuzzi", "Minibar", "TV Smart"],
-    imagen: "./assets/images/habitacion familiar.webp"
+    imagen: "./assets/images/habitaciones/habitacion familiar.webp",
+    bathroom: "./assets/images/baños/tina-regadera-1.jpeg",
+    bar:"./assets/images/mini-bar/bar-1.jpeg"
   },
 
   {
@@ -18,8 +20,10 @@ const habitaciones = [
     personas: 2,
     baño: "Regadera",
     precio: 180,
+    descripcion: "Perfecta para parejas o viajes cortos. Ofrece comodidad, privacidad y un ambiente tranquilo.",
     servicios: ["Wifi", "Aire acondicionado"],
-    imagen: "./assets/images/habitcion doble.jpg"
+    imagen: "./assets/images/habitaciones/habitcion doble.jpg",
+    bathroom: "./assets/images/baños/regadera-1.png"
   },
 
   {
@@ -27,10 +31,13 @@ const habitaciones = [
     nombre: "Habitación Matrimonial",
     camas: 1,
     personas: 2,
-    baño: "Tina",
+    baño: "Tina y regadera",
     precio: 250,
+    descripcion: "Diseñada para una experiencia romántica y confortable. Incluye detalles elegantes y excelente iluminación.",
     servicios: ["Wifi", "TV Smart", "Balcón"],
-    imagen: "./assets/images/habitacion matrimonial.png"
+    imagen: "./assets/images/habitaciones/habitacion matrimonial.png",
+    bathroom:"./assets/images/baños/baño 5.avif",
+    balcon:"./assets/images/balcon/balcon-1.webp"
   },
 
   {
@@ -40,8 +47,10 @@ const habitaciones = [
     personas: 1,
     baño: "Regadera",
     precio: 95,
+    descripcion: "Habitación sencilla y funcional para viajeros individuales. Ideal para descansar cómodamente.",
     servicios: ["Wifi", "Ventilador"],
-    imagen: "./assets/images/individual.jpg"
+    imagen: "./assets/images/habitaciones/individual.jpg",
+    bathroom: "./assets/images/baños/baño 3.jpg"
   },
 
   {
@@ -51,8 +60,13 @@ const habitaciones = [
     personas: 5,
     baño: "Tina y regadera",
     precio: 450,
+    descripcion: "Suite elegante con espacios exclusivos y acabados de lujo. Ideal para quienes buscan máximo confort.",
     servicios: ["Wifi", "Jacuzzi", "Minibar", "Sala privada"],
-    imagen: "./assets/images/suite-premium.jpg"
+    imagen: "./assets/images/habitaciones/dormitorio-de-lujo-moderno.jpg",
+    bathroom: "./assets/images/baños/baño 1.jpeg",
+    bar:"./assets/images/mini-bar/bar-2.jpg",
+    sala:"./assets/images/sala/sala-1.jpg"
+
   },
 
   {
@@ -62,8 +76,11 @@ const habitaciones = [
     personas: 3,
     baño: "Regadera",
     precio: 210,
+    descripcion: "Perfecta para grupos pequeños o amigos. Cuenta con camas cómodas y excelente distribución del espacio.",
     servicios: ["Wifi", "TV", "Escritorio"],
-    imagen: ".assets/images/habitacion-triple.jpg"
+    imagen: "./assets/images/habitaciones/hab-triple-1.jpg",
+    bathroom: "./assets/images/baños/regadera-2.jpeg",
+    escritorio:"./assets/images/sala/escritorio-1.jpeg"
   },
 
   {
@@ -73,8 +90,11 @@ const habitaciones = [
     personas: 2,
     baño: "Tina",
     precio: 370,
+    descripcion: "Habitación moderna pensada para viajeros de negocios. Incluye área de trabajo y ambiente elegante.",
     servicios: ["Wifi", "Oficina", "Cafetera"],
-    imagen: "./assets/images/habitacion ejecutiva.jpg"
+    imagen: "./assets/images/habitaciones/habitacion ejecutiva.jpg",
+    bathroom:"./assets/images/baños/tina-regadera-2.jpg",
+    escritorio:"./assets/images/sala/escritorio-2.jpeg"
   },
 
   {
@@ -85,7 +105,9 @@ const habitaciones = [
     baño: "Tina y regadera",
     precio: 390,
     servicios: ["Wifi", "Jacuzzi", "TV Smart", "Vista al mar"],
-    imagen: "./assets/images/habitacion r.webp"
+    imagen: "./assets/images/habitaciones/habitacion r.webp",
+    bathroom:"./assets/images/baños/tina-regadera-4.avif",
+    balcon:"./assets/images/balcon/balcon-2.jpeg"
   },
 
   {
@@ -95,8 +117,10 @@ const habitaciones = [
     personas: 2,
     baño: "Regadera",
     precio: 120,
+    descripcion: "Opción accesible y cómoda para estadías cortas. Incluye los servicios esenciales para descansar.",
     servicios: ["Wifi"],
-    imagen: "./assets/images/habitacion economica.avif"
+    imagen: "./assets/images/habitaciones/habitacion economica.avif",
+    bathroom:"./assets/images/baños/regadera-4.jpg"
   },
 
   {
@@ -106,8 +130,10 @@ const habitaciones = [
     personas: 2,
     baño: "Tina",
     precio: 340,
+    descripcion: "Habitación acogedora con decoración especial y ambiente íntimo. Ideal para parejas.",
     servicios: ["Wifi", "Jacuzzi", "Decoración especial"],
-    imagen: "./assets/images/habitacion 2.jpg"
+    imagen: "./assets/images/habitaciones/habitacion 2.jpg",
+    bathroom: "./assets/images/baños/baño 2.jpg"
   },
 
   {
@@ -117,8 +143,10 @@ const habitaciones = [
     personas: 2,
     baño: "Regadera",
     precio: 230,
+    descripcion: "Suite cómoda y moderna con excelente iluminación natural. Perfecta para una estadía relajante.",
     servicios: ["Wifi", "TV Smart", "Aire acondicionado"],
-    imagen: "./assets/images/habitacion 1.jpg"
+    imagen: "./assets/images/habitaciones/habitacion 1.jpg",
+    bathroom:"./assets/images/baños/tina-regadera-7.jpg"
   },
 
   {
@@ -128,8 +156,12 @@ const habitaciones = [
     personas: 3,
     baño: "Tina",
     precio: 410,
+    descripcion: "Habitación elegante con acabados modernos y vista panorámica. Ideal para descansar con estilo.",
     servicios: ["Wifi", "Minibar", "Vista panorámica"],
-    imagen: "./assets/images/habitacion king.jpg"
+    imagen: "./assets/images/habitaciones/habitacion king.jpg",
+    bathroom: "./assets/images/baños/tina-regadera-5.jpg",
+    balcon:"./assets/images/balcon/balcon-3.jpeg",
+    bar:"./assets/images/mini-bar/bar-3.webp"
   },
 
   {
@@ -139,8 +171,12 @@ const habitaciones = [
     personas: 6,
     baño: "Tina y regadera",
     precio: 780,
+    descripcion: "Suite exclusiva con espacios amplios y servicios premium. Diseñada para una experiencia de lujo.",
     servicios: ["Wifi", "Piscina privada", "Jacuzzi", "Cocina"],
-    imagen: "./assets/images/habitacion precidencial.jpg"
+    imagen: "./assets/images/habitaciones/habitacion precidencial.jpg",
+    bathroom:"./assets/images/baños/tina-regadera-3.jpg",
+    bar:"./assets/images/mini-bar/bar-4.avif",
+    balcon:"./assets/images/balcon/balcon-4.jpg"
   },
 
   {
@@ -150,8 +186,10 @@ const habitaciones = [
     personas: 4,
     baño: "Regadera",
     precio: 80,
+    descripcion: "Habitación práctica y espaciosa para compartir. Ideal para grupos o viajes económicos.",
     servicios: ["Wifi", "Lockers"],
-    imagen: "./assets/images/doble.webp"
+    imagen: "./assets/images/habitaciones/doble.webp",
+    bathroom:"./assets/images/baños/regadera-3.webp"
   },
 
   {
@@ -161,29 +199,12 @@ const habitaciones = [
     personas: 5,
     baño: "Tina y regadera",
     precio: 500,
+    descripcion: "Amplia suite familiar con áreas cómodas y modernas. Perfecta para disfrutar en grupo.",
     servicios: ["Wifi", "Cocina", "TV Smart", "Balcón"],
-    imagen: "./assets/images/king.avif"
+    imagen: "./assets/images/habitaciones/king.avif",
+    bathroom:"./assets/images/baños/tina-regadera-6.jpg",
+    balcon:"./assets/images/balcon/balcon-5.webp",
+    bar:"./assets/images/mini-bar/bar-5.webp"
   }
 ];
-habitaciones.forEach(habitacion => {
 
-  contenedor.innerHTML += `
-  
-    <div class="card">
-
-      <img src="${habitacion.imagen}">
-
-      <h2>${habitacion.nombre}</h2>
-
-      <p>${habitacion.camas} camas</p>
-
-      <p>Capacidad: ${habitacion.personas}</p>
-
-      <p>$${habitacion.precio}</p>
-
-      <button>Reservar</button>
-
-    </div>
-
-  `
-})
